e64e05fb1988386926bf5c28e107461d84cb96d4
freder <freder.code@gmail.com>
2015-10-25 14:10:58 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = {
-	task: require('./task/index.js')
+	task: require('./task/index.js'),
+	scraping: require('./scraping/index.js'),
+	parsing: require('./parsing/index.js'),
+	formatting: require('./formatting/index.js'),
 };
